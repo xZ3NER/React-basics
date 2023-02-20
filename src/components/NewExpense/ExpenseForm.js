b70991bc901888009eedd8c1c3c129d6ee8917e2
Peng You <ypenggg11@gmail.com>
2023-02-20 16:01:03 +0000

    Minor update: userInput state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [userInput, setUserInput] = useState({
+    titleInput: "",
+    amountInput: "",
+    dateInput: "",
+  });
+
+  const titleChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, titleInput: event.target.value };
+    });
+  };
+
+  const amountChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, amountInput: event.target.value };
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, dateInput: event.target.value };
+    });
+  };
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
-          <label>Title</label>
-          <input type='text' />
+          <label htmlFor='form-title'>Title</label>
+          <input
+            type='text'
+            id='form-title'
+            name='title-input'
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
-          <label>Amount</label>
-          <input type='text' />
+          <label htmlFor='form-amount'>Amount</label>
+          <input
+            type='text'
+            id='form-amount'
+            name='amount-input'
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
-          <label>Title</label>
-          <input type='number' min='0.01' step='0.01' />
-        </div>
-        <div className='new-expense__control'>
-          <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <label htmlFor='form-date'>Date</label>
+          <input
+            type='date'
+            id='form-date'
+            name='date-input'
+            min='2019-01-01'
+            max='2022-12-31'
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
